2005cdfbe728b1548cdf5154ba77dbc23a8c9e1a
Manuel Gallegos <m.gallegos@uniandes.edu.co>
2021-03-11 20:23:47 -0500

    Actualizacoin re1 2

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -142,7 +142,7 @@ public class Controller {
 				{
 					view.printMessage("El título es: "+ resp2.getTitle());
 					view.printMessage("El canal es: "+ resp2.getChannel());
-					view.printMessage("La categoría es: "+ resp2.getCategoryId());
+					view.printMessage("El Id de la categoría es: "+ resp2.getCategoryId());
 					view.printMessage("El número de días son: "+ modelo.darDiasTendencia());
 					pa = "";
 				}
